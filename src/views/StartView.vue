a34e8e57cfedfc084a7b29f2f97148bb393c56c2
Ygor <ygorpb2@gmail.com>
2023-03-21 00:25:44 -0300

    Criação Home e Start

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,20 +1,26 @@
-<script setup></script>
+<script setup>
+import ButtonComponent from "../components/ButtonComponent.vue";
+</script>
 
 <template>
   <main class="c">
     <img class="c__item" src="src/assets/img/image.png" alt="" />
     <section class="c-menu">
       <h1>Desafio MSB</h1>
-      <h1 class="c__item">Escolha alguns usuários abaixo 👇</h1>
-      <RouterLink to="/login">login</RouterLink>
-      <RouterLink to="/home">home</RouterLink>
+      <h1>TO DO LSIT</h1>
+      <div class="c__link">
+        <ButtonComponent rota="login" text="Get Started"></ButtonComponent>
+      </div>
     </section>
   </main>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
 .c {
+  flex-direction: column;
+  text-align: center;
+  background-color: #F2F5FF;
   &-menu {
-    background-color: white;
+    background-color: #F2F5FF;
     height: 250px;
     border-top-left-radius: 30px;
     border-top-right-radius: 30px;
@@ -22,13 +28,23 @@
     bottom: 80px;
     display: flex;
     text-align: center;
-    align-items: center;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-evenly;
+
     h1 {
       font-size: 16pt;
     }
+
+    :nth-child(2) {
+      font-weight: 700;
+    }
   }
+
+  &__link {
+    display: flex;
+    justify-content: center;
+  }
+
   &__item {
     width: 100%;
   }
