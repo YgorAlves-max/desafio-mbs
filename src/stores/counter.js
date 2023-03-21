d582fcd42470e60f8fc672183f1b8d685c5a7fd6
Ygor <ygorpb2@gmail.com>
2023-03-20 23:28:25 -0300

    Desenv requisições

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,6 +1,7 @@
-import axios from 'axios'
-import { defineStore } from 'pinia'
+import axios from 'axios';
+import { defineStore } from 'pinia';
 
+const API_URL = "https://6415ac7e351c4aed490c5c4f.mockapi.io/api/v1/";
 export const useCountStore = defineStore("state", {
   state: () => {
     return {
@@ -8,12 +9,11 @@ export const useCountStore = defineStore("state", {
     };
   },
   actions: {
-    async saveState() {
+    async userState() {
       try {
         await axios
-          .get(`https://6415ac7e351c4aed490c5c4f.mockapi.io/api/v1/tasks/`)
+          .get(API_URL + `/users`)
           .then((res) => {
-            console.log(res);
             this.data = res
             return res;
           });
@@ -21,5 +21,49 @@ export const useCountStore = defineStore("state", {
         this.alertErro = true;
       }
     },
+    async taskStaste() {
+      try {
+        await axios
+          .get(API_URL + `/tasks`)
+          .then((res) => {
+            this.data = res
+            return res;
+          });
+      } catch (error) {
+        this.alertErro = true;
+      }
+    },
+    registerUser(payload) {
+      try {
+        axios.post(API_URL + `/users`, payload)
+      }
+      catch (error) {
+        this.alertErro = true
+      }
+    },
+    registerTasks(payload) {
+      try {
+        axios.post(API_URL + `/tasks`, payload)
+      }
+      catch (error) {
+        this.alertErro = true
+      }
+    },
+    atulizarTasks(atualizarPayload,id) {
+      try {
+        axios.put(API_URL + `/tasks/${id}`, atualizarPayload)
+      }
+      catch (error) {
+        this.alertErro = true
+      }
+    },
+    deleteTasks(id) {
+      try {
+        axios.delete(API_URL + `/tasks/${id}`)
+      }
+      catch (error) {
+        this.alertErro = true
+      }
+    }
   },
 });
